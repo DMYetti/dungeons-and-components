8b94ec067bad8fee4c90e8a9db69878aad67a422
Christopher Baker <christopher@bitovi.com>
2019-11-25 23:07:05 -0800

    update description and note

--- src/components/description/description.styled.js
+++ src/components/description/description.styled.js
@@ -1,34 +1,26 @@
 import styled from '@emotion/styled';
+import { withBreakable } from '../../utils/styled';
 
 import border from './assets/border.png';
 
 export const Container = styled.div`
-  display: block-inline;
-  margin-bottom: 1em;
-  background-color: #faf7ea;
+  ${withBreakable}
+
   font-family: ScalySans;
+  margin: 0.125in 0;
+  padding: 0 4px;
+  background-color: #faf7ea;
   border-style: solid;
   border-width: 7px;
   border-image: url(${border}) 12 stretch;
   border-image-outset: 4px;
-  box-shadow: 0px 0px 6px #faf7ea;
+  box-shadow: 0 0.0625in 0.125in #888;
+  box-shadow: 0 0 0.0625in #faf7ea;
 
-  column-break-inside: avoid;
-  page-break-inside: avoid;
-  break-inside: avoid;
-
-  p {
-    line-height: 1.5em;
+  > :first-child {
+    margin-top: 0;
   }
-
-  em {
-    font-family: ScalySans;
-    font-style: italic;
-  }
-
-  strong {
-    font-family: ScalySans;
-    font-weight: 800;
-    letter-spacing: -0.02em;
+  > :last-child {
+    margin-bottom: 0;
   }
 `;
--- src/components/note/note.styled.js
+++ src/components/note/note.styled.js
@@ -1,31 +1,32 @@
 import styled from '@emotion/styled';
+import { withBreakable } from '../../utils/styled';
 
 import border from './assets/border.png';
 
 export const Container = styled.div`
+  ${withBreakable}
+
   font-family: ScalySans;
-  box-sizing: border-box;
-  margin: 2em 0;
-  padding: 5px 10px;
+  margin: 0.25in 0;
+  margin: calc(0.125in + 9px) 0;
   background-color: #e0e5c1;
   border-style: solid;
   border-width: 11px;
   border-image: url(${border}) 11;
   border-image-outset: 9px 0px;
-  box-shadow: 1px 4px 14px #888;
+  box-shadow: 0 0.0625in 0.125in #888;
 
-  column-break-inside: avoid;
-  page-break-inside: avoid;
-  break-inside: avoid;
-
-  p, ul {
-    font-size: 0.352cm;
+  > :first-child {
+    margin-top: 0;
+  }
+  > :last-child {
+    margin-bottom: 0;
   }
 `;
 
-export const Title = styled.h5`
+export const Title = styled.h4`
   margin-top: 0;
-  margin-bottom: 0.2em;
+  margin-bottom: 0.125in;
   font-family: ScalySansSmallCaps;
   font-size: 0.423cm;
   font-weight: 900;
