a09cd415e683657507d7963b6c2c2834750089a8
Christopher J Baker <christopher@hmudesign.com>
2021-06-13 19:17:36 -0700

    storybook, bable, typescript all works!

--- .storybook/preview.js
+++ .storybook/preview.js
@@ -1,22 +1,9 @@
 import { themes } from "@storybook/theming"
 
 export const parameters = {
-  layout: "centered",
   option: {
-    // storySort: a[1].kind === b[1].kind ? 0 : a[1].id.localeCompare(b[1].id, undefined, { numeric: true }),
-    // storySort: (a, b) => {
-    //   if (a[1].kind.endsWith('/README')) {
-    //     return -1
-    //   }
-    //   if (b[1].kind.endsWith('/README')) {
-    //     return 1
-    //   }
-
-    //   return a[1].id.localeCompare(b[1].id)
-    // },
     storySort: {
       method: 'alphabetical',
-      order: ['README'],
       locales: 'en-US',
     },
   },
--- src/components/description/Description.stories.tsx
+++ src/components/description/Description.stories.tsx
@@ -6,9 +6,21 @@ import { withHalfPage } from "../../helpers/stories"
 import Description, { DescriptionProps } from "./Description"
 
 export default {
-  title: "Components|Description",
+  title: "Components/Description",
   component: Description,
   decorators: [withHalfPage],
+  argTypes: {
+    theme: {
+      table: {
+        disable: true,
+      },
+    },
+    as: {
+      table: {
+        disable: true,
+      },
+    },
+  },
 } as Meta
 
 export const Basic: Story<DescriptionProps> = (args) => (
