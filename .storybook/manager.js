ab5b71a349763d6d63de0f5352e88c4d8b8b92cf
Christopher J Baker <christopher@hmudesign.com>
2021-06-15 00:20:09 -0700

    theme

--- .storybook/manager.js
+++ .storybook/manager.js
@@ -1,7 +1,7 @@
 import { addons } from '@storybook/addons'
-import { themes } from '@storybook/theming'
+
+import theme from "./theme"
 
 addons.setConfig({
-  theme: themes.dark,
-  panelPosition: 'bottom',
+  theme,
 })
